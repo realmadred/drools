337e2ee1ffe870ffa41a5cd5d17c580d0ebad0a3
liufeng <liufeng2008168@163.com>
2018-04-08 18:18:56 +0800

    drools

---
+++ src/test/java/com/feng/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.feng;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession("helloWorldSession");
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+}
